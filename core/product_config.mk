1daa3a57988cabbf0539115487744897811e1e22
cphelps76 <cphelps76@gmail.com>
2013-06-05 16:53:22 -0500

    DEMENTED

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(CARBON_BUILD),)
-    all_product_configs := $(shell ls device/*/$(CARBON_BUILD)/carbon.mk)
+  ifneq ($(DEMENTED_BUILD),)
+    all_product_configs := $(shell ls device/*/$(DEMENTED_BUILD)/DEMENTED.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # CARBON_BUILD
+  endif # DEMENTED_BUILD
 endif
 
-ifeq ($(CARBON_BUILD),)
+ifeq ($(DEMENTED_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
--- envsetup.sh
+++ envsetup.sh
@@ -61,12 +61,12 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^carbon_") ; then
-       CARBON_BUILD=$(echo -n $1 | sed -e 's/^carbon_//g')
+    if (echo -n $1 | grep -q -e "^DEMENTED_") ; then
+       DEMENTED_BUILD=$(echo -n $1 | sed -e 's/^DEMENTED_//g')
     else
-       CARBON_BUILD=
+       DEMENTED_BUILD=
     fi
-    export CARBON_BUILD
+    export DEMENTED_BUILD
 
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
@@ -456,7 +456,7 @@ function print_lunch_menu()
     echo
     echo "You're building on" $uname
     echo
-    if [ "z${CARBON_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${DEMENTED_DEVICES_ONLY}" != "z" ]; then
        echo "Breakfast menu... pick a combo:"
     else
        echo "Lunch menu... pick a combo:"
@@ -470,7 +470,7 @@ function print_lunch_menu()
         i=$(($i+1))
     done
 
-    if [ "z${CARBON_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${DEMENTED_DEVICES_ONLY}" != "z" ]; then
        echo "... and don't forget the bacon!"
     fi
 
@@ -481,7 +481,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka carbon
+        mka DEMENTED
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -492,10 +492,10 @@ function brunch()
 function breakfast()
 {
     target=$1
-    CARBON_DEVICES_ONLY="true"
+    DEMENTED_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/carbon/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/DEMENTED/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -511,8 +511,8 @@ function breakfast()
             # A buildtype was specified, assume a full device name
             lunch $target
         else
-            # This is probably just the CARBON model name
-            lunch carbon_$target-userdebug
+            # This is probably just the DEMENTED model name
+            lunch DEMENTED_$target-userdebug
         fi
     fi
     return $?
@@ -647,8 +647,8 @@ function tapas()
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=$(get_build_var CARBON_VERSION)
-        ZIPFILE=carbon-$MODVERSION.zip
+        MODVERSION=$(get_build_var DEMENTED_VERSION)
+        ZIPFILE=DEMENTED-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
@@ -663,7 +663,7 @@ function eat()
             done
             echo "Device Found.."
         fi
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.DEMENTED.device=$DEMENTED_BUILD");
     then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
@@ -688,7 +688,7 @@ EOF
     fi
     return $?
     else
-        echo "The connected device does not appear to be $CARBON_BUILD, run away!"
+        echo "The connected device does not appear to be $DEMENTED_BUILD, run away!"
     fi
 }
 
@@ -1274,7 +1274,7 @@ function lska() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                carbon|otapackage|systemimage)
+                DEMENTED|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
