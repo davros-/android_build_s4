ab5870ecbf0c378f72dbf02d2d573224428ce2f4
davros <davros@droidconcepts.com>
2013-06-12 23:00:25 -0400

    bye bye

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(DEMENTED_BUILD),)
-    all_product_configs := $(shell ls device/*/$(DEMENTED_BUILD)/demented.mk)
+  ifneq ($(Droid_Concepts_BUILD),)
+    all_product_configs := $(shell ls device/*/$(Droid_Concepts_BUILD)/Droid_Concepts.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # DEMENTED_BUILD
+  endif # Droid_Concepts_BUILD
 endif
 
-ifeq ($(DEMENTED_BUILD),)
+ifeq ($(Droid_Concepts_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
